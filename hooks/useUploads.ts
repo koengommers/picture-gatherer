9b22ede226c0139011dcf68a5472bb73d91464cf
Koen Gommers <koen@koengommers.nl>
2022-09-15 20:51:02 +0200

    Save image ids to localStorage

--- hooks/useUploads.ts
+++ hooks/useUploads.ts
@@ -1,9 +1,10 @@
-import { useReducer } from 'react'
+import buildUrl from 'cloudinary-build-url'
+import { useEffect, useReducer, useState } from 'react'
 
 type UploadStatus = 'pending' | 'uploading' | 'done'
 
 type Upload = {
-  file: File,
+  file?: File,
   status: UploadStatus,
   preview: string
 }
@@ -11,6 +12,9 @@ type Upload = {
 type UploadState = Upload[]
 
 type UploadAction = {
+  type: 'SET_UPLOADS',
+  payload: Upload[]
+} | {
   type: 'ADD_UPLOADS',
   payload: File[]
 } | {
@@ -20,12 +24,26 @@ type UploadAction = {
   type: 'UPLOAD_FINISHED',
   payload: {
     filename: string,
-    url: string
+    publicId: string
   }
 }
 
+const buildThumbnailUrl = (publicId: string) => buildUrl(publicId, {
+  cloud: {
+    cloudName: process.env.NEXT_PUBLIC_CLOUD_NAME,
+  },
+  transformations: {
+    resize: {
+      type: 'scale',
+      width: 200
+    }
+  }
+})
+
 const uploadReducer = (state: UploadState, action: UploadAction): UploadState => {
   switch (action.type) {
+    case 'SET_UPLOADS':
+      return action.payload
     case 'ADD_UPLOADS':
       const newUploads = action.payload.map(file => ({
         file,
@@ -35,7 +53,7 @@ const uploadReducer = (state: UploadState, action: UploadAction): UploadState =>
       return [...state, ...newUploads]
     case 'UPLOAD_START':
       return state.map(upload => {
-        if (upload.file.name === action.payload) {
+        if (upload.file?.name === action.payload) {
           return {
             ...upload,
             status: 'uploading'
@@ -45,11 +63,10 @@ const uploadReducer = (state: UploadState, action: UploadAction): UploadState =>
     })
     case 'UPLOAD_FINISHED':
       return state.map(upload => {
-        if (upload.file.name === action.payload.filename) {
+        if (upload.file?.name === action.payload.filename) {
           return {
-            ...upload,
             status: 'done',
-            preview: action.payload.url
+            preview: action.payload.publicId
           }
         }
         return upload
@@ -59,6 +76,7 @@ const uploadReducer = (state: UploadState, action: UploadAction): UploadState =>
 
 const useUploads = () => {
   const [state, dispatch] = useReducer(uploadReducer, [])
+  const [loaded, setLoaded] = useState(false)
 
   const addUploads = (name: string, files: File[]) => {
     dispatch({
@@ -80,23 +98,45 @@ const useUploads = () => {
         body: data
       })
       const result = await response.json()
+      const publicId = result.public_id
       if (response.ok) {
         dispatch({
           type: 'UPLOAD_FINISHED',
           payload: {
             filename: file.name,
-            url: result.secure_url
+            publicId
           }
         })
       }
     })
   }
 
+  const completedUploads = state.filter(upload => upload.status === 'done')
+  const completedUploadsEncoded = JSON.stringify(completedUploads)
+
+  useEffect(() => {
+    const uploadsEncoded = localStorage.getItem('uploads')
+    if (uploadsEncoded) {
+      const uploads = JSON.parse(uploadsEncoded)
+      dispatch({ type: 'SET_UPLOADS', payload: uploads })
+    }
+    setLoaded(true)
+  }, [])
+
+  useEffect(() => {
+    if (loaded) {
+      localStorage.setItem('uploads', completedUploadsEncoded)
+    }
+  }, [loaded, completedUploadsEncoded])
+
   const isUploading = state.some(upload => upload.status !== 'done')
-  const uploadCount = state.filter(upload => upload.status === 'done').length
+  const uploadCount = completedUploads.length
 
   return {
-    uploads: state,
+    uploads: state.map(upload => upload.status === 'done' ? {
+      ...upload,
+      preview: buildThumbnailUrl(upload.preview)
+    } : upload),
     isUploading,
     uploadCount,
     addUploads,
